57d4eb6a78959f39ce6d7e507e64d27116127536
trm187 <trm187@inlumine.ual.es>
2023-06-22 17:02:09 +0200

    changed e2e tests url

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,15 +1,15 @@
 describe('login', () => {
   it('login correct', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
-    cy.get('#emailInp').type('tesi2@gmail.com');
+    cy.get('#emailInp').type('tesi123@gmail.com');
     cy.get('#passInp').type('123456');
     cy.get('[type="submit"]').click();
     cy.get('.buttons-last-slot > :nth-child(2)').click();
   })
 
   it('login incorrect', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
     cy.get('#emailInp').type('tesi2@gmail.com');
     cy.get('#passInp').type('123456787');
@@ -18,7 +18,7 @@ describe('login', () => {
   })
 
   it('login incorrect - bad email', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
     cy.get('#emailInp').type('tesi2gmail.com');
     cy.get('#passInp').type('123456787');
@@ -30,7 +30,7 @@ describe('login', () => {
 
 describe('register', () => {
   it('register correct', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
     cy.get('#goToRegister').click();
     cy.get('#emailInp').type('cypres3333s@test.com');
@@ -40,7 +40,7 @@ describe('register', () => {
   });
 
   it('register incorrect - bad email', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
     cy.get('.sc-ion-buttons-md-h > :nth-child(2)').click();    
     cy.get('#emailInp').type('cypresstest.com');
@@ -49,7 +49,7 @@ describe('register', () => {
   });
 
   it('register incorrect - bad password', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
     cy.get('.sc-ion-buttons-md-h > :nth-child(2)').click();    
     cy.get('#emailInp').type('cypress@test');
@@ -58,7 +58,7 @@ describe('register', () => {
   });
 
   it('register incorrect - bad password and email', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
     cy.get('.sc-ion-buttons-md-h > :nth-child(2)').click();    
     cy.get('#emailInp').type('cypresstest');
@@ -70,16 +70,16 @@ describe('register', () => {
 
 describe('insert song', () => {
   it('insert song correct', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
-    cy.get('#emailInp').type('tesi2@gmail.com');
+    cy.get('#emailInp').type('tesi123@gmail.com');
     cy.get('#passInp').type('123456');
     cy.get('[type="submit"]').click();
     cy.get('.buttons-last-slot > :nth-child(1)').click();
     cy.get('#titleInp').type('test');
     cy.get('#artistInp').type('test');
     cy.get('#albumInp').type('test');
-    cy.get('#dateInp').type('2023-06-17');
+    cy.get('#dateInp').type('2023-06-22');
     cy.get('#genreInp').type('test');
     cy.get('#durationInp').type('2');
     cy.get('#urlInp').type('test');
@@ -93,14 +93,14 @@ describe('insert song', () => {
 describe('song search', () => {
 
   it('search by name correct', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('#ion-input-0').type('test');
     cy.get('#searchBtn').click();
     cy.get('ion-card-title.ion-inherit-color').contains('test');
   });
 
   it('search by artist correct', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get(':nth-child(2) > .ng-untouched').click();
     cy.get('#ion-input-0').type('test');
     cy.get('#searchBtn').click();
@@ -108,22 +108,22 @@ describe('song search', () => {
   });
 
   it('search by date correct', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get(':nth-child(3) > .ng-untouched').click();
-    cy.get('#ion-input-1').type('2023-06-17');
+    cy.get('#ion-input-1').type('2023-06-22');
     cy.get('#searchBtn').click();
     cy.get(':nth-child(1) > ion-card-header.ion-inherit-color > ion-card-title.ion-inherit-color').contains('asdf');
   });
 
   it('search by name incorrect', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('#ion-input-0').type('una cancion que no existe');
     cy.get('#searchBtn').click();
     cy.get('h1').contains('There are no songs to display');
   });
 
   it('search by artist incorrect', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get(':nth-child(2) > .ng-untouched').click();
     cy.get('#ion-input-0').type('un artista que no existe');
     cy.get('#searchBtn').click();
@@ -131,7 +131,7 @@ describe('song search', () => {
   });
 
   it('search by date incorrect', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get(':nth-child(3) > .ng-untouched').click();
     cy.get('#ion-input-1').type('3023-06-18');
     cy.get('#searchBtn').click();
@@ -139,7 +139,7 @@ describe('song search', () => {
   });
 
   it('search on Spotify', () => {
-    cy.visit('http://localhost:8100/home');
+    cy.visit('http://spotiapp.tesirm.tech/home');
     cy.get('.buttons-last-slot > .md').click();
     cy.get('#emailInp').type('tesi2@gmail.com');
     cy.get('#passInp').type('123456');
